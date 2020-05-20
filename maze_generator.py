34fb10ebd7ed635d91ca7d392e4d5009f2bb43fb
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-05-20 19:25:54 -0400

    Main main_generation function

--- maze_generator.py
+++ maze_generator.py
@@ -11,18 +11,23 @@ def generate_maze(size: int) -> np.array
     <size> : An integer representing the size of the maze to be generated,
     i.e size = 3 means a 3x3 maze.
     """
-    # create the maze array
+    # create the maze array, full of walls (0)
     maze = np.zeros((size, size))
-    visited = {}
 
+    rand_x = random.randint(0, len(maze) - 1)
+    rand_y = random.randint(0, len(maze) - 1)
     # start at a random cell in the maze
-    cell = maze[random.randint(0, len(maze) - 1,
-                random.randint(0, len(maze) - 1)]
-    visited[cell] = 1
-    
-    # select a random neightbouring cell that has not been visited
-    
+    cell = maze[rand_y, rand_x]
+    maze[rand_y, rand_x] = 1
+    wall_list = []
 
+    while len(wall_list) > 0:
+        # pick a random wall
+        rand_wall = wall_list[random.randint(0, len(wall_list) - 1)]
+        
+def get_cell_walls(position: Tuple[int, int):
+    """Given a position, return the position of the four cells 
+    surrounding that position"""
 
 if __name__ == '__main__':
     maze = generate_maze(10)
