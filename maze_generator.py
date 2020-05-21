3d5095c008c60c113329a3003af217dce605e4d2
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-05-21 13:40:18 -0400

    Completed the get_cell_walls function

--- maze_generator.py
+++ maze_generator.py
@@ -1,10 +1,9 @@
 import numpy as np
-from stack import Stack
 import random
 from typing import Tuple
 from PIL import Image
 
-def generate_maze(size: int) -> np.array
+def generate_maze(size: int) -> np.array:
     """ Return a numpy array that represents a maze. The maze is generated 
     using 'Prim's Algorithm'. 
 
@@ -19,17 +18,32 @@ def generate_maze(size: int) -> np.array
     # start at a random cell in the maze
     cell = maze[rand_y, rand_x]
     maze[rand_y, rand_x] = 1
-    wall_list = []
+    wall_list = get_cell_walls((rand_x, rand_y))
 
     while len(wall_list) > 0:
         # pick a random wall
         rand_wall = wall_list[random.randint(0, len(wall_list) - 1)]
-        
-def get_cell_walls(position: Tuple[int, int):
+        print(rand_wall) 
+    return maze
+
+def get_cell_walls(position: Tuple[int, int]):
     """Given a position, return the position of the four cells 
-    surrounding that position"""
+    surrounding that position, return the tuple as coordinates (x, y)"""
+    adjacent_cells = [
+    (position[0], position[1] - 1),
+    (position[0], position[1] + 1),
+    (position[0] - 1, position[1]),
+    (position[0] + 1, position[1])
+    ]
+    for cell in adjacent_cells:
+        if (cell[0] < 0) or (cell[1] < 0):
+            adjacent_cells.remove(cell)
+        elif (cell[0] > len(maze) - 1) or (cell[1] > len(maze) - 1):
+            adjacent_cells.remove(cell)
+    return adjacent_cells
 
 if __name__ == '__main__':
     maze = generate_maze(10)
+    print(maze)
     maze_image = Image.fromarray(maze, mode='L')
     maze_image.save('maze_100.png')
